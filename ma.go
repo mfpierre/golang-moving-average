4de79628c80dac71d600d2d5b7dfe56b4f144b66
Pierre <mfpierre@gmail.com>
2017-06-12 00:23:39 +0200

    Expose IsWindowFilled and gofmt

--- ma.go
+++ ma.go
@@ -4,19 +4,19 @@ package movingaverage
 // Moving average implementation for Go
 
 type MovingAverage struct {
-	Window int
-	values []float64
-	valPos int
-	slotsFilled bool
+	Window         int
+	values         []float64
+	valPos         int
+	IsWindowFilled bool
 }
 
 func (ma *MovingAverage) Avg() float64 {
 	var sum = float64(0)
-	var c = ma.Window-1
+	var c = ma.Window - 1
 
 	// Are all slots filled? If not, ignore unused
-	if !ma.slotsFilled {
-		c = ma.valPos-1
+	if !ma.IsWindowFilled {
+		c = ma.valPos - 1
 		if c < 0 {
 			// Empty register
 			return 0
@@ -43,16 +43,16 @@ func (ma *MovingAverage) Add(val float64) {
 	ma.valPos = (ma.valPos + 1) % ma.Window
 
 	// Did we just go back to 0, effectively meaning we filled all registers?
-	if !ma.slotsFilled && ma.valPos == 0 {
-		ma.slotsFilled = true
+	if !ma.IsWindowFilled && ma.valPos == 0 {
+		ma.IsWindowFilled = true
 	}
 }
 
 func New(window int) *MovingAverage {
 	return &MovingAverage{
-		Window : window,
-		values : make([]float64, window),
-		valPos : 0,
-		slotsFilled : false,
+		Window:         window,
+		values:         make([]float64, window),
+		valPos:         0,
+		IsWindowFilled: false,
 	}
 }
